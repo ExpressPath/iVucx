5a07721f5255bb74575a0ea4c18de81d4c2f70c7
ExpressPath <pdx.error@gmail.com>
2026-06-10 21:41:31 +0900

    j

--- api/helper.js
+++ api/helper.js
@@ -6,7 +6,7 @@ import {
   proxyHelperRequest,
   sendMethodNotAllowed
 } from '../lib/helper-proxy.js';
-import { sendPersistedProblemResponse } from '../lib/problem-store.js';
+import { sendPersistedProblemResponse, sendProblemPersistenceStatusResponse } from '../lib/problem-store.js';
 
 function getRouteSegments(req) {
   const raw = req && req.query ? req.query.route : undefined;
@@ -87,6 +87,11 @@ export default async function handler(req, res) {
       return;
     }
     await sendPersistedProblemResponse(req, res);
+    return;
+  }
+
+  if (segments.length === 1 && segments[0] === 'persist-check') {
+    await sendProblemPersistenceStatusResponse(req, res);
     return;
   }
 
